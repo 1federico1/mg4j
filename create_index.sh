7acc133254e270f0c4aaab8f8c10989691a9ec6f
1federico1 <federico.arcangeli1@gmail.com>
2017-03-16 11:17:17 +0100

    adjusted lines of code

--- create_index.sh
+++ create_index.sh
@@ -9,19 +9,17 @@ source set-my-classpath.sh
 
 echo "choose the collection folder"
 read collection
-echo "you have choosen $collection"
 
 to_append=".collection"
 
 collection_file="$collection$to_append"
-
-echo "new name is $collection_file"
 
 html_factory="it.unimi.di.big.mg4j.document.FileSetDocumentCollection -f HtmlDocumentFactory -p encoding=UTF-8 $collection_file"
 
 echo "Creating the collection"
 
 find $collection -iname \*.html | java $html_factory
+
 #Create the index
 # no memory optimization (-Xmx)
 
@@ -30,12 +28,14 @@ echo "Creating the index. It may require some time"
 build_index="it.unimi.di.big.mg4j.tool.IndexBuilder --downcase -S $collection_file $collection"
 
 java $build_index 
+
 #Querying the index
 
 text="-text"
 title="-title"
 coltext=$collection$text
 coltitle=$collection$title
+
 echo "Now you can query the index you just built! You can use the console or go to http://localhost:4242/Query from
 your browser"
 
